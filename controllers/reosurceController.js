9ffa65e62fec749edde248288bfe17847acf9011
kdrm <kdonram@gmail.com>
2025-03-14 16:20:11 +0000

    Real-time search for resources functionality.

--- controllers/reosurceController.js
+++ controllers/reosurceController.js
@@ -22,4 +22,21 @@ export const addResource = async (req, res) => {
       console.error('Error adding resource:', error);
       res.status(500).json({ message: 'Something went wrong' });
     }
-  };
+};
+
+//Real-time search for resources
+export const searchResources = async (req, res) => {
+    const { query } = req.query; 
+  
+    try {
+      const resources = await Resource.find({
+        availability: true, 
+        name: { $regex: query, $options: 'i' }, 
+      }).limit(10);
+  
+      res.status(200).json(resources);
+    } catch (error) {
+      console.error('Error searching resources:', error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
