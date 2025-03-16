3e9c8b201b7aa39b47faf10b5cfb4557bc889e9c
Reshin0608 <2reshin@gmail.com>
2025-03-16 06:49:15 +0000

    Get student's Schedule.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -21,3 +21,21 @@ export const addEventToSchedule = async (req, res) => {
         res.status(500).json({ message: 'Something went wrong' });
     }
 };
+
+// Get student's schedule
+export const getStudentSchedule = async (req, res) => {
+    const studentId = req.user._id;
+  
+    try {
+        const schedule = await Schedule.findOne({ studentId }).populate('studentId', 'name email');
+        if (!schedule) {
+            return res.status(404).json({ message: 'Schedule not found' });
+        }
+    
+        res.status(200).json(schedule);
+    } catch (error) {
+        console.error('Error fetching schedule:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
