bbc5a69b9cee404de4bf0de57197a14ecbaba06b
Reshin0608 <2reshin@gmail.com>
2025-03-16 06:49:56 +0000

    Update event in schedule.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -39,3 +39,34 @@ export const getStudentSchedule = async (req, res) => {
     }
 };
 
+// Update event in schedule
+export const updateEventInSchedule = async (req, res) => {
+    const studentId = req.user._id; 
+    const { eventId } = req.params; 
+    const { title, description, date, location, type } = req.body;
+  
+    try {
+      const schedule = await Schedule.findOne({ studentId });
+      if (!schedule) {
+        return res.status(404).json({ message: 'Schedule not found' });
+      }
+  
+      const event = schedule.events.id(eventId);
+      if (!event) {
+        return res.status(404).json({ message: 'Event not found' });
+      }
+  
+      if (title !== undefined) event.title = title;
+      if (description !== undefined) event.description = description;
+      if (date !== undefined) event.date = date;
+      if (location !== undefined) event.location = location;
+      if (type !== undefined) event.type = type;
+  
+      await schedule.save();
+  
+      res.status(200).json({ message: "Your schedule has been updated.",schedule});
+    } catch (error) {
+      console.error('Error updating event in schedule:', error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
