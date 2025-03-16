fafe6f7ad249606e9808dc8dfbd267d6fa878977
Reshin0608 <2reshin@gmail.com>
2025-03-16 06:48:40 +0000

    Add event to Schedule.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,1 +1,23 @@
 import Schedule from '../models/Schedule.js';
+
+// Add Event to Schedule
+export const addEventToSchedule = async (req, res) => {
+    const studentId = req.user._id;
+    const { title, description, date, location, type } = req.body;
+  
+    try {
+        let schedule = await Schedule.findOne({ studentId });
+    
+        if (!schedule) {
+            schedule = await Schedule.create({ studentId, events: [] });
+        }
+    
+        schedule.events.push({ title, description, date, location, type });
+        await schedule.save();
+  
+      res.status(201).json(schedule);
+    } catch (error) {
+        console.error('Error adding event to schedule:', error);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
