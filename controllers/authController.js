dc912b5492a0b116e36cac1f60467b60a24c9424
Gouri Nimalasiri <134169188+gourinimalasiri@users.noreply.github.com>
2025-03-18 23:12:37 +0000

    Added user registration functionality with email uniqueness check, auto-generated user IDs, password hashing, and JWT token generation.

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+
+// Register a new user
+export const register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const latestUser = await User.find().sort({_id: -1}).limit(1);
+      let id;
+      if (latestUser.length == 0) {
+        id = 'U-0001';
+      } else {
+        const currentId = latestUser[0].id;
+        let number = currentId.replace('U-','');
+        number = (parseInt(number, 10) + 1).toString().padStart(4, "0")
+        id = "U-" + number;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({ id, name, email, password: hashedPassword, role });
+
+    const token = jwt.sign({_id: user._id, id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};    
