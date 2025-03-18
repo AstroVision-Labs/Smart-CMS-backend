f25e32abccc926e1de5c665ce0758b0df60194e9
Gouri Nimalasiri <134169188+gourinimalasiri@users.noreply.github.com>
2025-03-18 21:32:10 +0000

    Functionality added to fetch all users and fetch a user by ID

---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+// Get all users
+export const getUsers = async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  };
+  
+  // Get a single user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({id: req.params.id});
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
