d2aef9b71b154b15fe91ff7e8641790fc6e85ed3
kdrm <kdonram@gmail.com>
2025-03-18 16:52:06 +0000

    Set storage engine.

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -1,3 +1,13 @@
 // utils/multerConfig.js
 import multer from 'multer';
 import path from 'path';
+
+// Set storage engine
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // Save files in the 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`); // Rename file to avoid conflicts
+    },
+});
